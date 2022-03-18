004bde1b2bbea285d8c0a7cf74f922ed87920339
AnastasssiaK <kulynychnastya@gmail.com>
2022-03-18 17:06:33 +0200

    ad and cw 12 done

--- lesson 12/additional/ad.js
+++ lesson 12/additional/ad.js
@@ -6,6 +6,74 @@
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(usersData => {
-        console.log(usersData);
-    })
+    .then(users => {
+        // console.log(usersData);
+        let usersBox = document.createElement('div');
+        usersBox.classList.add('usersBox');
+        document.body.appendChild(usersBox);
+        for (const user of users) {
+            let userBox = document.createElement('div');
+            let content = document.createElement('div');
+            content.classList.add('content');
+            userBox.classList.add('userBox');
+            userBox.innerHTML = `<h4>${user.id}. ${user.name}</h4><p>Username: ${user.username}<br>Email: ${user.email}<br>Address: ${user.address.city}</p><p>Phone: ${user.phone}<br>Website: ${user.website}<br>Company: ${user.company.name}</br>`;
+
+            let postsBtn = document.createElement('button');
+            postsBtn.classList.add('postsBtn');
+            postsBtn.innerText = 'Posts';
+            postsBtn.onclick = (e) => {
+                e.preventDefault();
+                postsBtn.disabled = true;
+                let postsBox = document.createElement('div');
+                postsBox.classList.add('postsBox');
+                content.appendChild(postsBox);
+                fetch('https://jsonplaceholder.typicode.com/posts')
+                    .then(response => response.json())
+                    .then(posts => {
+                        // console.log(posts);
+                        for (const post of posts) {
+                            if (post.userId === user.id) {
+                                let postBox = document.createElement('div');
+                                postBox.classList.add('postBox');
+                                postBox.innerHTML = `
+                                <h3>${post.id}. ${post.title}</h3>
+                                <div>${post.body}</div>
+                                `;
+                                let commentsBtn = document.createElement('button');
+                                commentsBtn.classList.add('commentsBtn');
+                                commentsBtn.innerText = 'Comments';
+                                postsBox.append(postBox, commentsBtn);
+                                commentsBtn.onclick = (e) => {
+                                    e.preventDefault();
+                                    let commentsBox = document.createElement('div');
+                                    commentsBox.classList.add('commentsBox');
+                                    postBox.appendChild(commentsBox);
+                                    fetch('https://jsonplaceholder.typicode.com/comments')
+                                        .then(response => response.json())
+                                        .then(comments => {
+                                            // console.log(comments);
+                                            for (const comment of comments) {
+                                                if (comment.postId === post.userId) {
+                                                    let commentBox = document.createElement('div');
+                                                    commentBox.classList.add('commentBox');
+                                                    commentBox.innerHTML = `
+                                                       <h4>${comment.id}. ${comment.name}</h4>
+                                                       <p><b>Email:</b> ${comment.email}</p>
+                                                       <p><b>Body:</b>${comment.body}</p>
+                                                       `;
+                                                   commentsBox.appendChild(commentBox);
+                                                }
+                                            }
+                                        });
+                                    commentsBtn.disabled = true;
+                                }
+                            }
+                        }
+                    });
+            }
+
+            userBox.appendChild(postsBtn);
+            content.append(userBox);
+            usersBox.appendChild(content);
+        }
+    });
--- lesson 12/classwork/cw.js
+++ lesson 12/classwork/cw.js
@@ -4,51 +4,61 @@
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
 
 fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => console.log(response.json()))
-    // .then(postsData => {
-    //     console.log(postsData);
-    //     let posts = document.createElement('div');
-    //     posts.classList.add('posts');
-    //     let h1 = document.createElement('h1');
-    //     h1.innerText = 'Posts';
-    //     for (const postItem of postsData) {
-    //         let post = document.createElement('div');
-    //         post.classList.add('post');
-    //         let idPost = document.createElement('div');
-    //         idPost.classList.add('idPost');
-    //         idPost.innerHTML = `<h3>${postItem.id}</h3>`;
-    //         let postBody = document.createElement('div');
-    //         postBody.classList.add('postBody');
-    //         postBody.innerHTML = `<h4>${postItem.title}</h4><div>${postItem.body}</div>`;
-    //
-    //         let commentsBtn = document.createElement('button');
-    //         commentsBtn.classList.add('commentsBtn');
-    //         commentsBtn.innerText = 'Усі коментарі';
-    //         commentsBtn.addEventListener('click', function (e) {
-    //             e.preventDefault()
-    //             fetch('https://jsonplaceholder.typicode.com/comments')
-    //                 .then(response => response.json())
-    //                 .then(commentsData => {
-    //                     console.log(commentsData);
-    //                     for (const commentItem of commentsData) {
-    //                         if (postItem.userId === commentItem.postId) {
-    //                             let commentsAll = document.createElement('div');
-    //                             commentsAll.classList.add('commentsAll');
-    //                             commentsAll.innerHTML = `
-    //                                     <h3>Id: ${commentsData.id}</h3>
-    //                                     <h4>Name: ${commentsData.name}</h4>
-    //                                     <p>Email: ${commentsData.email}</p>
-    //                                     <div>Body: ${commentsData.body}</div>
-    //                                     `;
-    //                             post.appendChild(commentsAll)
-    //                         }
-    //                     }
-    //                 });
-    //         });
-    //
-    //         post.append(idPost, postBody, commentsBtn);
-    //         posts.append(post);
-    //     }
-    //     document.body.append(h1, posts);
-    // });
+    .then(response => response.json())
+    .then(postsData => {
+        // console.log(postsData);
+        let posts = document.createElement('div');
+        posts.classList.add('posts');
+        let h1 = document.createElement('h1');
+        h1.innerText = 'Posts';
+        for (const postItem of postsData) {
+            let post = document.createElement('div');
+            post.classList.add('post');
+            let idPost = document.createElement('div');
+            idPost.classList.add('idPost');
+            idPost.innerHTML = `<h3>${postItem.id}</h3>`;
+            let postBody = document.createElement('div');
+            postBody.classList.add('postBody');
+            postBody.innerHTML = `<h4>${postItem.title}</h4><div>${postItem.body}</div>`;
+
+            let commentsBtn = document.createElement('button');
+            commentsBtn.classList.add('commentsBtn');
+            commentsBtn.innerText = 'Усі коментарі';
+            commentsBtn.addEventListener('click', function (e) {
+                e.preventDefault();
+                fetch('https://jsonplaceholder.typicode.com/comments')
+                    .then(response => response.json())
+                    .then(commentsData => {
+                        // console.log(commentsData);
+                        for (const commentItem of commentsData) {
+                            if (commentItem.postId === postItem.id) {
+                                let commentsAll = document.createElement('div');
+                                commentsAll.classList.add('commentsAll');
+                                commentsAll.innerHTML = `
+                                        <h3>Id: ${commentItem.id}</h3>
+                                        <h4>Name: ${commentItem.name}</h4>
+                                        <p>Email: ${commentItem.email}</p>
+                                        <div>Body: ${commentItem.body}</div>
+                                        `;
+                                postBody.appendChild(commentsAll)
+                            }
+                        }
+                    });
+                // let close = document.createElement('button');
+                // close.classList.add('closeBtn');
+                // close.innerText = 'X';
+                // close.onclick = (e) => {
+                //     e.preventDefault();
+                // let comments = document.getElementsByClassName('commentsAll');
+                //     comments.classList.add('close')
+                //     close.classList.add('close');
+                // }
+                // postBody.appendChild(close);
+            });
+            postBody.append(commentsBtn)
+            post.append(idPost, postBody);
+            posts.append(post);
+        }
+        document.body.append(h1, posts);
+    });
 
